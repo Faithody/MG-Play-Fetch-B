6997c520d41e22ddd26d3f2a6ab7c8508e8baf1f
Faithody <pistisodiase@gmail.com>
2024-09-19 14:15:37 +0100

    Add files via upload

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float slowDown=10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        slowDown -= 0.1f;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && slowDown <= 0.0f)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            slowDown = 10.0f;    
+        }
+    }
+}
